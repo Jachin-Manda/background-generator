7797b55d5e837dd1429774dbd5fcd09e22aa61c8
Jachin-github <65728679+Jachin-github@users.noreply.github.com>
2020-10-04 17:03:47 +0200

    Added Random color button on webpage

--- script.js
+++ script.js
@@ -2,18 +2,57 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.getElementById("randomcolor");
+var rndcolor1 = Math.floor(Math.random() * 255);
+var rndcolor2 = Math.floor(Math.random() * 255);
 
-function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
+// console.log(css);00
+// console.log(color1);
+// console.log(color2);
+
+function changeGradient() {
+	body.style.background = "linear-gradient(to right, " 
 	+ color1.value 
-	+ ", " 
+	+ "," 
 	+ color2.value 
 	+ ")";
 
-	css.textContent = body.style.background + ";";
+	css.textContent = body.style.background + ";"; //shows color values set
+}
+function randomColor() {
+  color1.value = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  color2.value = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  changeGradient();
 }
 
-color1.addEventListener("input", setGradient);
+button.addEventListener("click", randomColor);
 
-color2.addEventListener("input", setGradient);
+color1.addEventListener("input", changeGradient);
+
+color2.addEventListener("input", changeGradient);
+
+
+// var css = document.querySelector("h3");
+// var color1 = document.querySelector(".color1");
+// var color2 = document.querySelector(".color2");
+// var body = document.getElementById("gradient");
+
+// function setGradient() {
+// 	body.style.background = 
+// 	"linear-gradient(to right, " 
+// 	+ color1.value 
+// 	+ ", " 
+// 	+ color2.value 
+// 	+ ")";
+
+// 	css.textContent = body.style.background + ";";
+// }
+
+// color1.addEventListener("input", setGradient);
+
+// color2.addEventListener("input", setGradient);
+
+
+
+
+
